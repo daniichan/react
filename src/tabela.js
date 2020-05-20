5bc419dec9228b2892236ea9ea425ca280cf643c
Daniele Leite <danii.exe@gmail.com>
2020-05-19 22:45:03 -0300

    3.5. Método de remoção

--- src/tabela.js
+++ src/tabela.js
@@ -21,7 +21,7 @@ const TableBody = props => {
                 <td>{ linha.livro }</td>
                 <td>{ linha.preco }</td>
                 <td>
-                    <button>Remover</button>
+                    <button onClick={() => { props.removeAutor(index) }}>Remover</button>
                 </td>
             </tr>
         );
@@ -37,12 +37,12 @@ const TableBody = props => {
 
 class Tabela extends Component {
     render() {
-        const { autores } = this.props;
+        const { autores, removeAutor } = this.props;
 
         return(
             <table>
                 <TableHead />
-                <TableBody autores={ autores } />
+                <TableBody autores={ autores } removeAutor={ removeAutor } />
             </table>
         );
     }        
