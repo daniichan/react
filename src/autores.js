dc40087dcaf6d7a6503d72a500b58437457e9c6a
Daniele Leite <danii.exe@gmail.com>
2020-05-24 12:44:52 -0300

    3.8. Solução para a página de Autores e de Livros

--- src/autores.js
+++ src/autores.js
@@ -1,13 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Header from './header';
+import DataTable from './datatable';
 
-const Autores = () => {
-    return(
-        <Fragment>
-            <Header />
-            <h1>Autores</h1>
-        </Fragment>
-    );
+class Autores extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            autores: [
+                {
+                    autor: 'Paulo',
+                },
+                {
+                    autor: 'Daniel',
+                },
+                {
+                    autor: 'Marcos',
+                },
+                {
+                    autor: 'Bruno',
+                },
+                {
+                    autor: 'Nico',
+                }
+            ],
+            titulo: 'Autores'
+        }
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <Header />
+                <div className='container'>
+                    <h1>Autores</h1>
+                    <DataTable
+                        dados={this.state.autores}
+                        titulo={this.state.titulo}
+                        colunas={['autor']}
+                    />
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Autores;
--- src/livros.js
+++ src/livros.js
@@ -1,13 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './header';
+import DataTable from './datatable';
 
-const Livros = () => {
-    return(
-        <Fragment>
-            <Header />
-            <h1>Livros</h1>
-        </Fragment>
-    );
+class Livros extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            livros: [
+                {
+                    livro: 'React'
+                },
+                {
+                    livro: 'Java'
+                },
+                {
+                    livro: 'Design'
+                },
+                {
+                    livro: 'DevOps'
+                },
+                {
+                    livro: 'Java'
+                }
+            ],
+            titulo: 'Livros'
+        };
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Header />
+                <div className='container'>
+                    <h1>Livros</h1>
+                    <DataTable 
+                        dados={this.state.livros} 
+                        titulo={this.state.titulo} 
+                        colunas={['livro']}
+                    />
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Livros;
