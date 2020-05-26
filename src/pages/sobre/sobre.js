967361d496b42aadec77ef141fae918dc9c57fd3
Daniele Leite <danii.exe@gmail.com>
2020-05-26 18:50:43 -0300

    3.5. makeStyles

--- src/pages/sobre/sobre.js
+++ src/pages/sobre/sobre.js
@@ -2,16 +2,26 @@ import React, { Fragment } from 'react';
 import Header from '../../components/header/header';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const userEstilos = makeStyles({
+    titulo: {
+        textAlign: 'center',
+        color: 'blue'
+    }
+});
 
 const Sobre = () => {
+    const classes = userEstilos();
+
     return(
         <Fragment>
             <Header />
             <Container maxWidth="sm">
-                <Typography variant="h1" component="h2">Sobre</Typography>
-                <p>
+                <Typography className={classes.titulo} variant="h1" component="h2">Sobre</Typography>
+                <Typography variant="body1" component="p">
                     A Casa do Código é uma editora que desenvolve e edita livros em diversos formatos.
-                </p>
+                </Typography>
             </Container>
         </Fragment>
     );
