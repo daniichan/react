f0f486f53acd0576d40055cc5352557e54b2665f
Daniele Leite <danii.exe@gmail.com>
2020-05-26 17:12:53 -0300

    1.3. Instalando MaterialUI

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import App from './pages/home/home';
 import Sobre from './pages/sobre/sobre';
 import Livros from './pages/livros/livros';
 import Autores from './pages/autores/autores';
 import NotFound from './pages/notFound/notFound';
+import './index.css';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-
   <BrowserRouter>
     <Switch>
       <Route path='/' exact={true} component={App} />
@@ -26,8 +20,3 @@ ReactDOM.render(
   </BrowserRouter>
   , document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
